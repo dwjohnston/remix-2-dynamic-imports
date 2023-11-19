d99c5ab9e9a1dce8d528897ac09ea630ecad18d7
David Johnston <david@blacksheepcode.com>
2023-11-19 16:05:50 +1100

    Add dynamic import

---
+++ app/routes/$.tsx
@@ -0,0 +1,16 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
+
+export const loader = async (loaderInput:LoaderFunctionArgs) => {
+
+    const pathname = loaderInput.params["*"];
+    const result = await import(`../app/json/${pathname}.json`); 
+
+    
+    return result;
+}
+
+export default function () {
+
+
+    return <div>Whatever</div>
+}
